5af8ce4c0232d28a13a9bc42542d1fe62e63fa5c
TeBlanco <gio.cerda@duocuc.cl>
2024-04-19 15:18:50 +0000

    Radio 3

---
+++ R3.py
@@ -0,0 +1,15 @@
+import math 
+
+def calcular_C(radio):
+
+pi = round(match.pi, 6)
+
+circ = 2 * pi * radio
+
+return circ
+
+radio  3
+
+circ = calcular_C(radio)
+
+print ("pose un radio de ", radio, "cm", circ, "cm" )
